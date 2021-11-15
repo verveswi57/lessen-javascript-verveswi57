bebe8b0ca254c6c2527cb1974c86a066ba278be3
Willy Vervest <66535205+verveswi57@users.noreply.github.com>
2021-11-15 19:24:05 +0100

    besteging

--- mine/les1_introdution/main.js
+++ mine/les1_introdution/main.js
@@ -1,170 +1,164 @@
 // naming conventions
-let camelcase = 'test';
-const SCREAMING_CASE = 'test';
+//let camelcase = 'test';
+// const SCREAMING_CASE = 'test';
 // logging to console
-console.log(camelCase);
+// console.log(camelCase);
 
 // documentation
 
 // https://developper.mozilla.org/en-US/
 
 // primitives data types
-let boot = true;
-let otherBool = false;
+//let boot = true;
+//let otherBool = false;
 
 // number
 
-let number = 12;
-let otherNumber = 1.1;
+//let number = 12;
+//let otherNumber = 1.1;
 
 // string
 
-let string = 'test';
+//let string = 'test';
 
 // null
 
-let nullVal = null;
+// let nullVal = null;
 
 // undefined
 
-let undefinedVar = undefined;
-let otherUndefined;
+//let undefinedVar = undefined;
+//let otherUndefined;
 
 // check type
-console.log(typeof aNumber);
+//console.log(typeof aNumber);
 
 // type coercion
 
 // veriabelen
 
 //let
-let avariable = 'test';
+//let avariable = 'test';
 
 // const
-const A_CONST = 'test';
+//const A_CONST = 'test';
 
 // function
-function afuncion(aParameter){
-    // do something
-    return aParameter * 2;
-}
-let returnVal = aFunction(2);
-console.log(returnVal);
+//function afuncion(aParameter){
+// do something
+// return aParameter * 2;
+//}
+// let returnVal = aFunction(2);
+//console.log(returnVal);
 
-function aFunction(name){
-    let aNaam = name;
-    return name;
-}
-let returnVal = 'willy';
-console.log(returnVal);
+//function aFunction(name){
+//let aNaam = name;
+//return name;
+//}
+//let returnVal = 'willy';
+//console.log(returnVal);
 
 //  alert
-alert('hello willy');
+//alert('hello willy');
 
 //  prompt
-let name = prompt('test');
-console.log(name);
+//let name = prompt('test');
+//console.log(name);
 
-function helloNamePrompt(name) {
-    let name =  prompt('what is your name?');
-   console.log(name);
-   alert('hello' + name);
-}
+//function helloNamePrompt(name) {
+//let name =  prompt('what is your name?');
+//console.log(name);
+// alert('hello' + name);
+//}
 
 // string
 
-let someString = 'hier kan je tekst kwijt';
+//let someString = 'hier kan je tekst kwijt';
 
 //lenght
 
-console.log(someString.lenght);
+// console.log(someString.lenght);
 
 // toUpperCase & lowerCase
-console.log(someString.toUpperCase());
+//console.log(someString.toUpperCase());
 
 // substring()
 
-function getrierKan() {
-    return someStringtr.substring(1, 6);
-}
-console.log(getrierKan());
+//function getrierKan() {
+//return someStringtr.substring(1, 6);
+//}
+//console.log(getrierKan());
 
 // get chat on index
-console.log(someString([5]);
+//console.log(someString([5]);
 
 //  concat  string
 
-console.log(someString + ' ' + someMoreString);
+//console.log(someString + ' ' + someMoreString);
 
-console.log(12 + '2');
+//console.log(12 + '2');
 
-console.log(12 + 4 + '6');
+//console.log(12 + 4 + '6');
 
-someMoreString += ' nog wat tekst'
+//someMoreString += ' nog wat tekst'
 
-console.log(someMoreString);
+//console.log(someMoreString);
 
 // template literals
 
-let aTemplateLiteral = 'Test 123';
+//let aTemplateLiteral = 'Test 123';
 
-test 456;
-test 789;
+//test 456;
+//test 789;
 
-let templateString = 'template';
-let helloTemplate = `hello $(templatrString)`;
+//let templateString = 'template';
+//let helloTemplate = `hello $(templatrString)`;
 
 function helloNamePrompt(name) {
-    let name = prompt('What is your first name ?');
-    alert('hello + name');
+let name = prompt('What is your first name ?');
+alert('hello + name');
 }
 
 function capitalize(text) {
-   let firstLetter = text[0] ;
-   let firstLetterUpper = firstLetter.UpperCase();
-   let otherLetter = test.substring(i);
-   let otherLetterLower = otherLetterLower.toLowerCase(i);
+let firstLetter = text[0] ;
+let firstLetterUpper = firstLetter.UpperCase();
+let otherLetter = test.substring(i);
+let otherLetterLower = otherLetterLower.toLowerCase(i);
 
-   return firstLetterUpper + otherLetterLower;
+return firstLetterUpper + otherLetterLower;
 }
 
 function helloFullNamePromptCapitalize(name1) {
-    let firstName = prompt('What is your first name ?');
-    let firstNameCapitalize = capitalize(firstName);
-    let secondName = prompt('What is your second name ?');
-    let secondNameCapitalize = capitalize(secondName)
-    alert('hello + firstNameCapitalize + ' ' + secondNameCapitalize')
+let firstName = prompt('What is your first name ?');
+let firstNameCapitalize = capitalize(firstName);
+let secondName = prompt('What is your second name ?');
+let secondNameCapitalize = capitalize(secondName)
+alert('hello + firstNameCapitalize + ' ' + secondNameCapitalize')
 
 }
 
 function firstLast(name) {
-    let firstName = [0];
-    let firstNameUpper = firstName.UpperCase();
-    let otherLetter = test.substring(i);
-    return otherLetter + firstLetter;
+let firstName = [0];
+let firstNameUpper = firstName.UpperCase();
+let otherLetter = test.substring(i);
+return otherLetter + firstLetter;
 }
-
 
 function lastFirst(name) {
-    let lastFirst = word[word.lenght - 1];
-    let ohterLetter = word.substring((0, word.lenght - 1))
-    return lastLetter + orherLetter;
+let lastFirst = word[word.lenght - 1];
+let ohterLetter = word.substring((0, word.lenght - 1))
+return lastLetter + orherLetter;
 }
-
 
 function lastFirstLast(word) {
-    let firstLetter = word[0];
-    let lastLetter = word[word.length - 1];
-    let otherLetters = word.substring(1, word.length - 1);
+let firstLetter = word[0];
+let lastLetter = word[word.length - 1];
+let otherLetters = word.substring(1, word.length - 1);
 
-    return lastLetter + otherLetters + firstLetter;
+return lastLetter + otherLetters + firstLetter;
 }
-
 
 function ellipser(sentence, lenght) {
-    let shortSentence = sentence.substring(0, lenght);
-    return shortSentence + '...';
+let shortSentence = sentence.substring(0, lenght);
+return shortSentence + '...';
 }
-
-
-

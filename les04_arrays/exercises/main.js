04e563c959ef6f4953d3b3c89edf07b4fb76af68
Willy Vervest <willy.vervest@gmail.com>
2021-12-02 19:54:32 +0100

    Add files via upload

---
+++ les04_arrays/exercises/main.js
@@ -0,0 +1,55 @@
+/**
+ * Given an array, return the same array but make sure no value occurs more than once
+ * @param {array} anArray
+ * @return {array}
+ */
+function removeDoubles(anArray) {
+    return [1, 2, 3, 4];
+}
+
+console.log(removeDoubles([3, 5, 7, 12, 3, 6, 9, 10, 10, 5])); // [3, 5, 7, 12, 6, 9, 10]
+
+/**
+ * Given an array of numbers, return the same array containing only even numbers
+ * @param {number[]} anArray
+ * @return {number[]}
+ */
+function onlyEven(anArray) {}
+
+/**
+ * Sort an array from low to high, write the logic yourself
+ * @param {number[]} anArray
+ * @return {number[]}
+ */
+function sort(anArray) {}
+
+/**
+ * Reverse the string
+ * Try to keep the same casing (if first letter is
+ * uppercase, make sure it is still uppercase in the reversed word
+ * @param {string} aString
+ * @return {string}
+ */
+function reverseString(aString) {}
+
+/**
+ * Given two arrays of values, return an array with only the values that occur in both arrays
+ * @param {array} anArray
+ * @param {array} otherArray
+ * @return {array}
+ */
+function findSimilars(anArray, otherArray) {}
+
+/**
+ * Given an array return an array with the same values but in a random order
+ * @param anArray
+ */
+function scramble(anArray) {}
+
+/**
+ * Given two arrays, check if the second array (subArray) is a sublist of the first (bigArray)
+ * @param {array} bigArray
+ * @param {array} subArray
+ * @return {boolean}
+ */
+function isSublist(bigArray, subArray) {}

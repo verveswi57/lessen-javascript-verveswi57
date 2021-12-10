cb2e118804e23b83edfae37669887a4d97aaee2b
Willy Vervest <66535205+verveswi57@users.noreply.github.com>
2021-12-10 13:43:20 +0100

    tweestring

--- les07_browser/ex01-list/main.js
+++ les07_browser/ex01-list/main.js
@@ -18,3 +18,63 @@ let aContainer = document.getElementById('container');
 let items = ['item 1', 'item 2', 'item 3', 'item 4'];
 
 genList(aContainer, items);
+
+
+/**
+ * Generate a list
+ * @param {HTMLElement} container
+ * @param {Array} content
+ */
+function genList(container, content) {
+    let newUl = document.createElement('ul');
+
+    for (let i = 0; i < content.length; i++) {
+        let newLi = document.createElement('li');
+        newLi.innerText = content[i];
+        newUl.append(newLi);
+    }
+
+    container.append(newUl);
+}
+
+let aContainer = document.getElementById('container');
+let items = ['item 1', 'item 2', 'item 3', 'item 4'];
+
+genList(aContainer, items);
+
+
+/**
+ * Generate a list
+ * @param {HTMLElement} container
+ * @param {Array} content
+ */
+function genList(container, content) {
+    let newUl = document.createElement('ul');
+
+    if (array) { 
+      for (let i = 0; i < content.length; i++) {
+        let newLi = document.createElement('li');
+        newLi.innerText = content[i];
+        newUl.append(newLi);
+      } else {
+          for (let i = 0; i < content.length; i++) {
+        let newLi = document.createElement('li');
+        newLi.innerText = content[i];
+        newUl.append(newLi);
+
+
+
+      }
+
+
+
+    }
+
+    container.append(newUl);
+}
+
+let aContainer = document.getElementById('container');
+let items = ['item 1', 'item 2', 'item 3', 'item 4'];
+
+genList(aContainer, items);
+
